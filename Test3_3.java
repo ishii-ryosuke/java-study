7b60963410d743fb451558ebcf90a9baf4daa1b0
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-21 13:15:22 +0900

    練習問題の3-3,3-5,3-6を解きました

---
+++ Test3_3.java
@@ -0,0 +1,20 @@
+/**
+ * 【3-3】
+ * Hungryプログラム
+ */
+public class Test3_3 {
+    public static void main(String[] arg) {
+        int isHungry = 0;
+        String food = "お米";
+
+        System.out.println("こんにちは");
+
+        if (isHungry == 0) {
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+            System.out.println(food + "をいただきます");
+            System.out.println("ごちそうさまでした");
+        }
+    }
+}
