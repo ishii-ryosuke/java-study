65188297bda89150de8af6516ec083b9f7eb03b3
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-22 13:05:46 +0900

    練習問題4-4を解きました

---
+++ Test4_4.java
@@ -0,0 +1,20 @@
+/**
+ *【4-4】
+ * 数当てクイズver.2
+ */
+public class Test4_4 {
+    public static void main(String[] args) {
+        int[] numbers = {3, 4, 9};
+
+        System.out.println("1桁の数字を入力してください");
+
+        int input = new java.util.Scanner(System.in).nextInt();
+
+        for (int hitnumber : numbers) {
+            if(input == hitnumber) {
+                System.out.println("アタリ！");
+            }
+        }
+
+    }
+}
