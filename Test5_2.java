abd68595e484228ece238a8ae4129ee64824d1cc
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-22 14:36:40 +0900

    練習問題5-2を解きました

---
+++ Test5_2.java
@@ -0,0 +1,14 @@
+/**
+ * 【5-2】
+ * メソッド「email」
+ */
+public class Test5_2 {
+    public static void main(String[] args) {
+        email("挨拶", "000@email.com", "おはようございます");
+    }
+
+    public static void email(String title, String address, String text) {
+        System.out.println(address + "に、以下のメールを送信しました\n件名:" + title + "\n本文:" + text);
+    }
+
+}
