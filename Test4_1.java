cdba211a3b7688647eff339e4910d6da98e02ba6
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-22 13:04:53 +0900

    練習問題4-1を解きました

---
+++ Test4_1.java
@@ -0,0 +1,13 @@
+/**
+ * 【4-1】
+ * 条件を満たす各配列を準備せよ
+ */
+public class Test4_1 {
+    public static void main(String[] args) {
+
+        int[] points = new int[4];
+        double[] weights = new double[5];
+        boolean[] answers = new boolean[3];
+        String[] names = new String[3];
+    }
+}
