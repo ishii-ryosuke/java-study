2c16a084823c9eba0dedfafc144460295e0618df
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-23 09:44:47 +0900

    不要な改行を削除しました

--- Test4_1.java
+++ Test4_1.java
@@ -4,7 +4,6 @@
  */
 public class Test4_1 {
     public static void main(String[] args) {
-
         int[] points = new int[4];
         double[] weights = new double[5];
         boolean[] answers = new boolean[3];
