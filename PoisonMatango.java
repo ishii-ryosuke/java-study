3c0ba9ec792d02bbf5cb3186e8a072f8b0d2716c
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-27 16:52:08 +0900

    練習問題10-3解答

---
+++ PoisonMatango.java
@@ -0,0 +1,19 @@
+public class PoisonMatango extends Matango {
+    int poison = 5;
+
+    public PoisonMatango(char sufffix) {
+        super(sufffix);
+    }
+
+    public void attack(Hero h) {
+        super.attack(h);
+
+        if (this.poison != 0) {
+            System.out.println("さらに毒の胞子をばらまいた！");
+            int poisonDamage = h.hp / 5;
+            h.hp -= poisonDamage;
+            System.out.println(poisonDamage + "ポイントのダメージ！");
+            this.poison--;
+        }
+    }
+}
