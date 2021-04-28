e389251623b32d6818f8d9a1231bc5d2d4b220fc
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-04-28 16:30:12 +0900

    練習問題11-1~11-4解答

---
+++ Computer.java
@@ -0,0 +1,16 @@
+/**
+ * 【11-1】
+ * コンピュータのクラス、Computerクラス
+ */
+public class Computer extends TangibleAsset {
+    String makerName;
+
+    public Computer(String name, int price, String color, String makerName) {
+        super(name, price, color);
+        this.makerName = makerName;
+    }
+
+    public String getMakerName() {
+        return this.makerName;
+    }
+}
---
+++ Thing.java
@@ -0,0 +1,8 @@
+/**
+ * 【11-3】
+ * 重さを取得するインターフェース、Thing
+ */
+public interface Thing {
+    double getWeight();
+    void setWeight(double weight);
+}
