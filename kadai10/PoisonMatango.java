04f2d3393f1e81e398c9e0aac8bf3a65c7994b10
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-05-11 13:35:31 +0900

    変数hをpoisonedHeroに変更

--- kadai10/PoisonMatango.java
+++ kadai10/PoisonMatango.java
@@ -7,17 +7,17 @@ package kadai10;
 public class PoisonMatango extends Matango {
     int poison = 5;
 
-    public PoisonMatango(char monsterSymbol) {
-        super(monsterSymbol);
+    public PoisonMatango(char suffix) {
+        super(suffix);
     }
 
-    public void attack(Hero h) {
-        super.attack(h);
+    public void attack(Hero poisonedHero) {
+        super.attack(poisonedHero);
 
         if (this.poison != 0) {
             System.out.println("さらに毒の胞子をばらまいた！");
-            int poisonDamage = h.hp / 5;
-            h.hp -= poisonDamage;
+            int poisonDamage = poisonedHero.hp / 5;
+            poisonedHero.hp -= poisonDamage;
             System.out.println(poisonDamage + "ポイントのダメージ！");
             this.poison--;
         }
