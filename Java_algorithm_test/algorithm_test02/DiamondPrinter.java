3addcb832ba0c30e786a3de90e2ac8475354b09b
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-06-22 10:22:52 +0900

    コード整形・細かい修正

--- Java_algorithm_test/algorithm_test02/DiamondPrinter.java
+++ Java_algorithm_test/algorithm_test02/DiamondPrinter.java
@@ -12,42 +12,47 @@ public class DiamondPrinter {
 
     /**
      * 指定されたスペース数とドット数の文字列を返す.
+     *
      * @param space スペース数
-     * @param dot ドット数
+     * @param dot   ドット数
      * @return 指定されたスペース数とドット数の文字列
      */
-    private String createLine(int space, int dot) {
-        String diamondLine = "";
+    private StringBuilder createLine(int space, int dot) {
+        StringBuilder diamondLine = new StringBuilder();
+        StringBuilder spaceString = new StringBuilder();
+        StringBuilder dotString = new StringBuilder();
         for (int i = 0; i < space; i++) {
-            diamondLine += " ";
+            spaceString.append(" ");
         }
         for (int i = 0; i < dot; i++) {
-            diamondLine += "*";
+            dotString.append("*");
         }
-        for (int i = 0; i < space; i++) {
-            diamondLine += " ";
-        }
+        diamondLine.append(spaceString);
+        diamondLine.append(dotString);
+        diamondLine.append(spaceString);
+        diamondLine.append("\n");
         return diamondLine;
     }
 
     /**
      * 指定の高さのひし形を文字列で返す.
+     *
      * @param height 高さ
      * @return 指定の高さのひし形の文字列
      */
-    private String createDiamond(int height) {
-        String diamondString = "";
-        int diamondLine = 0;
-        for (; diamondLine < height ; diamondLine++) {
-            int dot = 2 * diamondLine + 1;
+    private StringBuilder createDiamond(int height) {
+        StringBuilder diamondString = new StringBuilder();
+        int diamondWidth = 0;
+        for (; diamondWidth < height; diamondWidth++) {
+            int dot = 2 * diamondWidth + 1;
             int space = ((2 * height - 1) - dot) / 2;
-            diamondString += createLine(space, dot) + "\n";
+            diamondString.append(createLine(space, dot));
         }
-        diamondLine--;
-        for (; diamondLine != 0; diamondLine--) {
-            int dot = 2 * diamondLine - 1;
-            int space = ((2 * height -1) - dot) / 2;
-            diamondString += createLine(space, dot) + "\n";
+        diamondWidth--;
+        for (; diamondWidth != 0; diamondWidth--) {
+            int dot = 2 * diamondWidth - 1;
+            int space = ((2 * height - 1) - dot) / 2;
+            diamondString.append(createLine(space, dot));
         }
         return diamondString;
     }
